8965f5643fa236aeb9b9267afae492f67473504a
Mahmoud Reda <55753782+MAHMOUDRR707@users.noreply.github.com>
2022-10-30 15:31:49 +0200

    Time: 352 ms (24.39%), Space: 93.3 MB (88.88%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+#include<algorithm>
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+     int max_x = 0;
+     int x = 0 ; 
+     int min_x =  prices[0] ;
+     for(auto &i : prices){
+         min_x =  min(i,min_x);
+         x =  i - min_x ;
+         max_x =  max(max_x,x);
+     }
+        return max_x;
+    }
+};
