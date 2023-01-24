5e4fb4f55719e7ff9900164f3f73d1f198751e60
Mahmoud Reda <55753782+MAHMOUDRR707@users.noreply.github.com>
2023-01-24 02:03:38 +0200

    Time: 30 ms (57.30%), Space: 13.5 MB (43.99%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -13,18 +13,21 @@ public:
             reverse(columns.begin(), columns.end());
         }
         vector<int> dist(n * n + 1, -1);
-        queue<int> q;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
         dist[1] = 0;
-        q.push(1);
+        q.emplace(0, 1);
         while (!q.empty()) {
-            int curr = q.front();
+            auto [d, curr] = q.top();
             q.pop();
+            if (d != dist[curr]) {
+                continue;
+            }
             for (int next = curr + 1; next <= min(curr + 6, n * n); next++) {
                 auto [row, column] = cells[next];
                 int destination = board[row][column] != -1 ? board[row][column] : next;
-                if (dist[destination] == -1) {
+                if (dist[destination] == -1 || dist[curr] + 1 < dist[destination]) {
                     dist[destination] = dist[curr] + 1;
-                    q.push(destination);
+                    q.emplace(dist[destination], destination);
                 }
             }
         }
